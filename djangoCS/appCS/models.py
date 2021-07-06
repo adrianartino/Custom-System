4b64ad38b5310fca28ff661801dd14631d3ebd34
BlancaGaeta <17231399@itslerdo.edu.mx>
2021-07-06 15:03:41 -0500

    Se hizo la conexion local con la BD

--- djangoCS/appCS/models.py
+++ djangoCS/appCS/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Empleados(models.Model):
+    id_empleado=models.AutoField(primary_key=True)
+    nombre=models.CharField(max_length=80)
+    apellidos=models.CharField(max_length=80)
---
+++ djangoCS/djangoCS/__init__.py
@@ -0,0 +1,3 @@
+#indicar que se esta usando el servicio de pymysql en django
+import pymysql
+pymysql.install_as_MySQLdb()
